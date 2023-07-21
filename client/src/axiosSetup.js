48b679d9d714fd846679f8e8757a27c2def92937
Adeyemi-Femi <bossfemzy@gmail.com>
2023-07-21 16:57:43 +0100

    added an update

--- client/src/axiosSetup.js
+++ client/src/axiosSetup.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = 'http://localhost:5000/api'
+const BASE_URL = 'https://christain-konnect.onrender.com/api'
 
 const privateRequest = axios.create({
     baseURL: BASE_URL,
