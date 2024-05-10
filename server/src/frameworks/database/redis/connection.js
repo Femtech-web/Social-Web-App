56f0d84bbf20f7db475fa2be352389b334081105
Akolade <bossfemzy10@gmail.com>
2024-05-10 03:27:50 -1200

    [feat]: added SEO features

--- server/src/frameworks/database/redis/connection.js
+++ server/src/frameworks/database/redis/connection.js
@@ -1,29 +1,20 @@
-import Redis from 'ioredis';
+import Redis from "ioredis";
 
 export default function connection(config) {
   const redisConnection = new Redis({
     port: config.redis.port,
-    host: config.redis.host
-  })
-
-  // redisConnection.ping((err, result) => {
-  //   if (err) {
-  //     console.error('Unable to connect to Redis:', err);
-  //   } else {
-  //     console.log('Connected to Redis. PING result:', result);
-  //   }
-  // });
-
-  redisConnection.on('connect', () => {
-    console.log('Connected to Redis!');
+    host: config.redis.host,
   });
 
-  redisConnection.on('error', (err) => {
+  redisConnection.on("connect", () => {
+    console.log("Connected to Redis!");
+  });
+
+  redisConnection.on("error", (err) => {
     console.log(`Error  ${err}`);
   });
 
   return {
-    redisConnection
+    redisConnection,
   };
 }
-
